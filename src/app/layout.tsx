12974ff7ef713c95ad1e3761b7210f005e5eff31
Joshua Sigston <joshua.sigston@outlook.com>
2025-06-27 12:32:07 -0700

    header, light/dark mode buttong

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Goat Notes",
@@ -21,7 +22,12 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className="flex min-h-screen w-full flex-col">
+            <Header />
+          </div>
+          <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+            {children}
+          </main>
           <Toaster />
         </ThemeProvider>
       </body>
---
+++ src/components/header.tsx
@@ -0,0 +1,51 @@
+import { shadow } from "@/styles/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import DarkModeToggle from "./dark-mode-toggle";
+
+const Header = () => {
+  const user = null;
+  return (
+    <header
+      className="relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+      style={{
+        boxShadow: shadow,
+      }}
+    >
+      <Link href="/" className="flex items-end gap-2">
+        <Image
+          src="/goatius.png"
+          height={60}
+          width={60}
+          alt="logo"
+          priority
+          className="rounded-full"
+        />
+        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+          GOAT <span>Notes</span>
+        </h1>
+      </Link>
+      <div className="flex gap-4">
+        {user ? (
+          "Logout"
+        ) : (
+          <>
+            <Button asChild>
+              <Link href="/sign-up" className="hidden sm:block">
+                Sign Up
+              </Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+        <DarkModeToggle />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
